e1ec896055f3c85d6d2d434a2d5de9f77a248ba3
Aman Gupta <guptaaman89630@gmail.com>
2025-01-20 01:33:57 +0530

    Model Trainer Evaluation and Mlflow Done

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-# Dataclass is used for creating vars for anempty classes
+# Dataclass is used for creating vars for an empty class
 
 @dataclass
 class DataIngestionArtifact:
@@ -20,3 +20,15 @@ class DataTransformationArtifact:
     transformed_object_file_path: str
     transformed_train_file_path: str
     transformed_test_file_path: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
