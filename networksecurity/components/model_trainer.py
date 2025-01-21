da7810b5168e21b89cfdab5796670dc0b9c007ac
Aman Gupta <guptaaman89630@gmail.com>
2025-01-21 19:18:03 +0530

    Pipeline Completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,8 @@ from sklearn.ensemble import (
 )
 
 import mlflow
+import dagshub
+dagshub.init(repo_owner='amangupta-pixel', repo_name='networksecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -109,7 +111,8 @@ class ModelTrainer:
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
         
-        
+        # model pusher
+        save_object("final_model/model.pkl",best_model)
 
         ## Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
